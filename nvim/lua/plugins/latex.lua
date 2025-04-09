beea39920fd56283ac112878304bc41c43ed65a6
Lukas <lucidLuckylee@users.noreply.github.com>
2025-04-09 14:03:36 +0200

    Configure forward search for zathura

--- nvim/lua/plugins/latex.lua
+++ nvim/lua/plugins/latex.lua
@@ -2,14 +2,33 @@ return {
 	{
 		"lervag/vimtex",
 		lazy = false, -- we don't want to lazy load VimTeX
-		-- tag = "v2.15", -- uncomment to pin to a specific release
 		init = function()
-			-- VimTeX configuration goes here, e.g.
+			-- VimTeX base configuration
 			vim.g.vimtex_view_method = "zathura"
+			vim.g.vimtex_view_forward_search_on_start = 1
+			vim.g.vimtex_view_forward_search_on_compile = 1
+			vim.g.vimtex_view_automatic = 1
 			vim.g.vimtex_complete_enabled = 1
+
 			vim.g.vimtex_compiler_latexmk = {
-				out_dir = '../build'
+				out_dir = '../build',
+				options = {
+					"-synctex=1",
+					"-interaction=nonstopmode",
+					"-file-line-error",
+				},
 			}
-		end
+
+			-- Make CursorHold trigger faster
+			vim.o.updatetime = 500 -- time in ms before CursorHold is triggered
+
+			-- Automatically trigger forward search when cursor stops moving
+			vim.api.nvim_create_autocmd("CursorHold", {
+				pattern = "*.tex",
+				callback = function()
+					vim.cmd("silent VimtexView") -- silently trigger forward search
+				end,
+			})
+		end,
 	}
 }
